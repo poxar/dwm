82cab66187554ddbc4ee23a7ee9ff9480977760d
Philipp Millar <philipp.millar@poxar.net>
2023-12-01 19:57:24 +0100

    Use return for zoom as well
    This makes it easy to use with all my keyboard layouts.

--- config.h
+++ config.h
@@ -110,6 +110,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_m,      setlayout,      {0} },
