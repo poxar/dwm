c2980cb8cc2f38d83d037611168e25add65ed390
Philipp Millar <philipp.millar@poxar.net>
2024-06-12 17:39:57 +0200

    Don't use rofi combi mode by default
    The combi disables the built-in history, having which is worth the
    slight inconvenience of switching modes.

--- config.h
+++ config.h
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "rofi", "-show", "combi", NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const char *actcmd[] = { "rofi", "-show", "dwm", "-modes", "dwm:Code/dwm/actions.sh", NULL };
