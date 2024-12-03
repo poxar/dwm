91fa1816c9f69cfc9de682455ed5225216e82821
Philipp Millar <philipp.millar@poxar.net>
2024-12-03 17:18:33 +0100

    Configuration changes ¯\_(ツ)_/¯

--- config.h
+++ config.h
@@ -2,26 +2,26 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int rmaster            = 0;        /* 1 means master-area is initially on the right */
+static const int rmaster            = 1;        /* 1 means master-area is initially on the right */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraCode Nerd Font Light:size=20" };
+static const char *fonts[]          = { "FiraCode Nerd Font Light:size=11" };
 
-static const char col_fg[] = "#f4e4e1";
+static const char col_fg[] = "#fafafa";
 static const char col_bg[] = "#000000";
-static const char col_hi[] = "#e3bcb5";
+static const char col_hi[] = "#6a6868";
 
 static const char *colors[][3]      = {
 	/*               fg      bg      border   */
-	[SchemeNorm] = { col_fg, col_bg, col_fg },
-	[SchemeSel]  = { col_bg, col_hi, col_hi },
+	[SchemeNorm] = { col_fg, col_bg, col_hi },
+	[SchemeSel]  = { col_fg, col_hi, col_bg },
 };
 
 /* tagging */
@@ -34,11 +34,10 @@ static const Rule rules[] = {
 	 */
 	/* class             instance    title       tags mask     iscentered   isfloating   monitor */
 	{ "Alacritty",       "screencast", NULL,     1 << 5,       0,           0,           -1 },
-	{ "firefox",         "Toolkit",  "Picture-in-Picture", (1<<9)-1,  0,    1,           -1 },
+	{ "firefox",         "Toolkit",  "Picture-in-Picture", (1<<9)-1,  0,    0,           -1 },
+	{ "firefox",         NULL,       NULL,       1 << 1,       0,           0,           -1 },
 	{ "steam",           NULL,       NULL,       1 << 6,       0,           0,           -1 },
-	{ "Audacious",       NULL,       NULL,       1 << 7,       0,           0,           -1 },
-	{ "Spotify",         NULL,       NULL,       1 << 7,       0,           0,           -1 },
-	{ "discord",         NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	{ "discord",         NULL,       NULL,       1 << 7,       0,           0,           -1 },
 	{ "TelegramDesktop", NULL,       NULL,       1 << 8,       0,           0,           -1 },
 	{ "Signal",          NULL,       NULL,       1 << 8,       0,           0,           -1 },
 };
@@ -73,8 +72,8 @@ static const char *actcmd[] = { "rofi", "-show", "dwm", "-modes", "dwm:Code/dwm/
 static const char *sesscmd[] = { "rofi", "-show", "sessions", "-modes", "sessions:Code/dwm/sessions.sh", NULL };
 static const char *notecmd[] = { "rofi", "-show", "notes", "-modes", "notes:Code/dwm/notes.sh", NULL};
 static const char *filecmd[] = { "rofi", "-show", "files", "-modes", "files:Code/dwm/files.sh", NULL};
-static const char *explcmd[] = { "thunar", NULL };
-static const char *musicmd[] = { "thunar", "Music", NULL };
+static const char *explcmd[] = { "alacritty", "-e", "lf", NULL };
+static const char *emojicmd[] = { "rofi", "-show", "emoji", "-modes", "emoji", NULL };
 
 #include "movestack.c"
 static const Key keys[] = {
@@ -87,7 +86,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_n,      spawn,          {.v = notecmd } },
 	{ MODKEY,                       XK_f,      spawn,          {.v = filecmd } },
 	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = explcmd } },
-	{ MODKEY|ShiftMask,             XK_m,      spawn,          {.v = musicmd } },
+	{ MODKEY,                       XK_a,      spawn,          {.v = emojicmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -97,6 +96,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_l,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_m,      setmfact,       {.f = 1.50 } },
+	{ MODKEY|ShiftMask,             XK_m,      setmfact,       {.f = 1.40 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ Mod1Mask,                     XK_Tab,    view,           {0} },
@@ -105,9 +106,9 @@ static const Key keys[] = {
 	{ MODKEY|Mod1Mask,              XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|Mod1Mask,              XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|Mod1Mask,              XK_f,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|Mod1Mask,              XK_r,      togglermaster,  {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_m,      togglermaster,  {0} },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
@@ -149,6 +150,7 @@ static const Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkClientWin,       MODKEY|ShiftMask, Button1,        resizemouse,    {0} },
+	{ ClkClientWin,       MODKEY|ShiftMask, Button2,        killclient,     {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
